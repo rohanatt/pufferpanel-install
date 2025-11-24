708f178f5e3d3ccacc4356f44587517199addab1
rohanatt <71555075+rohanatt@users.noreply.github.com>
2025-11-25 03:20:58 +0600

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 # FOR x86 servers using linux : 
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
-./ngrok config add-authtoken 2XWvdIQegnz7fRUXkMvJWVy7Z5p_3SijirFvadrRgBJot32fG #Put Yours here
+./ngrok config add-authtoken 2eh2nsZ2I4OxHCapmtY2Ab4FUYv_4iKVUKF8gGSfFpFtx43L8 #Put Yours here
 ./ngrok http 8080
